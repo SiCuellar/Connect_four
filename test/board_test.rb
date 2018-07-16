7efba9e7a24deab09c629193a8504fadc8b7ced1
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-07-16 15:55:28 -0600

    the start of the code

---
+++ test/board_test.rb
@@ -0,0 +1,28 @@
+require "./lib/board.rb"
+require "minitest/pride"
+require "minitest/autorun"
+
+class BoardTest < Minitest::Test
+
+  def test_it_exist
+    board = Board.new
+
+    assert_instance_of Board , board
+  end
+
+  def test_it_prints_board
+    
+
+
+
+  end
+
+
+
+
+
+
+
+
+
+end
