a40845ab5b152638ac02a1cd072ea77046be8c56
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-07-17 17:42:58 -0600

    functioning chip placement

--- lib/board.rb
+++ lib/board.rb
@@ -9,13 +9,13 @@ class Board
 
     @keys = ["A","B","C","D","E","F","G"]
     @board = [
-
-    [".",".",".",".",".",".","."],
-    [".",".",".",".",".",".","."],
-    [".",".",".",".",".",".","."],
-    [".",".",".",".",".",".","."],
-    [".",".",".",".",".",".","."],
-    [".",".",".",".",".",".","."]
+    [".",".",".",".",".","."],
+    [".",".",".",".",".","."],
+    [".",".",".",".",".","."],
+    [".",".",".",".",".","."],
+    [".",".",".",".",".","."],
+    [".",".",".",".",".","."],
+    [".",".",".",".",".","."],
     ]
   end
 
@@ -24,22 +24,19 @@ class Board
   def display_board
     puts @keys.join(" ")
 
-    @board.map do |line|
+    @board.transpose.map do |line|
       puts line.join(" ")
     end
   end
 
   def chip_place(letter)
     chip_col = @keys.index(letter)
-    flip_board[chip_col] << x_or_o
-    flip_board[chip_col].shift
-    binding.pry
-    @board
-  end
-
-  def flip_board
+    @board[chip_col] << "X"
+    @board[chip_col].shift
     @board.transpose
   end
+
+
 
 
 
